efdbdcec76c8946a9697cabcd6ad6eba71a2f663
joeuni <dmsdl8917@gmail.com>
2024-02-20 21:21:34 +0900

    트윗 작성란

--- src/App.vue
+++ src/App.vue
@@ -92,7 +92,40 @@ vue
         </div>
       </div>
     </div>
-    <div class="flex-1 bg-red-200"></div>
+    <div class="flex-1">
+      <div class="flex">
+        <!-- 메인 섹션 -->
+        <div class="flex-1 border-r border-gray-100">
+          <div class="flex flex-col">
+            <div class="border-b border-gray-100 px-3 py-2 font-bold text-lg">
+              홈
+            </div>
+            <!-- 트윗팅 섹션 -->
+            <div class="flex px-3 py-3 border-b-8 border-gray-100">
+              <img
+                src="http://picsum.photos/200"
+                class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"
+              />
+              <div class="ml-2 flex-1 flex flex-col">
+                <textarea
+                  placeholder="무슨 일이 일어나고 있나요?"
+                  class="w-full text-lg font-bold focus:outline-none mb-3 resize-none"
+                ></textarea>
+                <div class="text-right">
+                  <button
+                    class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full"
+                  >
+                    트윗
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 트랜드 섹션 -->
+        <div class="w-2/5">trend</div>
+      </div>
+    </div>
   </div>
 </template>
 
