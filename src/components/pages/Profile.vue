2ee648154f1925e505f7dccc9c17691d58077b13
joeuni <dmsdl8917@gmail.com>
2024-02-21 11:21:02 +0900

    프로필 완료

--- src/components/pages/Profile.vue
+++ src/components/pages/Profile.vue
@@ -14,6 +14,63 @@
           <div class="text-xs text-gray">232 트윗</div>
         </div>
       </div>
+      <!-- 프로필 백그라운드 이미지-->
+      <div class="bg-gray-300 h-40 relative">
+        <!-- 프로필이미지 -->
+        <div
+          class="w-28 h-28 border-4 border-white bg-gray-100 rounded-full absolute -bottom-14 left-2"
+        >
+          <img
+            src="http://picsum.photos/200"
+            class="rounded-full opacity-90 hover:opacity-100 cursor-pointer"
+          />
+        </div>
+      </div>
+      <!-- 프로필 수정 버튼 -->
+      <div class="text-right mr-2 mt-2">
+        <button
+          class="border border-primary text-primary px-3 py-2 hover:bg-blue-50 font-bold rounded-full text-sm"
+        >
+          프로필 수정
+        </button>
+      </div>
+      <!-- 유저 정보 -->
+      <div class="mx-3 mt-2">
+        <div class="font-extrabold text-lg">이름</div>
+        <div class="text-gray">아이디</div>
+        <div>
+          <span class="text-gray">가입일:</span>
+          <span class="text-gray">2011년 10월</span>
+        </div>
+        <div>
+          <span class="font-bold mr-1">28</span>
+          <span class="text-gray mr-3">팔로우 중</span>
+          <span class="font-bold mr-1">27</span>
+          <span class="text-gray">팔로워</span>
+        </div>
+      </div>
+      <div class="flex border-b border-color mt-3">
+        <div
+          class="py-3 w-1/4 text-primary font-bold border-b border-primary text-center hover:bg-blue-50 cursor-pointer hover:text-primary"
+        >
+          트윗
+        </div>
+        <div
+          class="py-3 w-1/4 text-gray font-bold text-center hover:bg-blue-50 cursor-pointer hover:text-primary"
+        >
+          트윗
+        </div>
+        <div
+          class="py-3 w-1/4 text-gray font-bold text-center hover:bg-blue-50 cursor-pointer hover:text-primary"
+        >
+          트윗
+        </div>
+        <div
+          class="py-3 w-1/4 text-gray font-bold text-center hover:bg-blue-50 cursor-pointer hover:text-primary"
+        >
+          트윗
+        </div>
+      </div>
     </div>
     <!-- 트랜드 섹션 -->
     <Trends />
