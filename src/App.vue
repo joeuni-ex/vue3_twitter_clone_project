fb3457311e8dd2c18c6c3fcf35dc8237cca011df
joeuni <dmsdl8917@gmail.com>
2024-02-21 11:02:32 +0900

    사이드 반응형 lg->xl

--- src/App.vue
+++ src/App.vue
@@ -3,88 +3,88 @@ vue
   <div class="flex h-screen container mx-auto">
     <!-- 사이드 섹션 -->
     <div
-      class="w-20 lg:w-1/4 pt-5 lg:ml-10 flex flex-col justify-between border-r border-gray-200"
+      class="w-20 xl:w-1/4 pt-5 xl:ml-10 flex flex-col justify-between border-r border-gray-200"
     >
-      <div class="flex flex-col items-center lg:items-start">
+      <div class="flex flex-col items-center xl:items-start">
         <!-- 트위터 로고 -->
-        <i class="fab fa-twitter text-3xl text-primary lg:ml-4 mb-3"></i>
+        <i class="fab fa-twitter text-3xl text-primary xl:ml-4 mb-3"></i>
         <!-- 트위터 사이드 메뉴 -->
         <div class="flex flex-col items-start space-y-1">
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="fas fa-home fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden lg:inline-block">홈</span>
+            <span class="ml-3 text-xl hidden xl:inline-block">홈</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="fas fa-hashtag fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden lg:inline-block">탐색하기</span>
+            <span class="ml-3 text-xl hidden xl:inline-block">탐색하기</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-bell fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden lg:inline-block">알림</span>
+            <span class="ml-3 text-xl hidden xl:inline-block">알림</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-envelope fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden lg:inline-block">쪽지</span>
+            <span class="ml-3 text-xl hidden xl:inline-block">쪽지</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-bookmark fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden lg:inline-block">북마크</span>
+            <span class="ml-3 text-xl hidden xl:inline-block">북마크</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-list-alt fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden lg:inline-block">리스트</span>
+            <span class="ml-3 text-xl hidden xl:inline-block">리스트</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="far fa-user fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden lg:inline-block">프로필</span>
+            <span class="ml-3 text-xl hidden xl:inline-block">프로필</span>
           </div>
           <div
             class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer"
           >
             <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
-            <span class="ml-3 text-lg hidden lg:inline-block">더보기</span>
+            <span class="ml-3 text-xl hidden xl:inline-block">더보기</span>
           </div>
         </div>
         <!-- 트윗 버튼 -->
-        <div class="w-full h-12 lg:pr-3 flex justify-center">
+        <div class="w-full h-12 xl:pr-3 flex justify-center">
           <button
-            class="mt-3 bg-primary text-white lg:w-full w-12 h-12 rounded-full hover:bg-dark"
+            class="mt-3 bg-primary text-white xl:w-full w-12 h-12 rounded-full hover:bg-dark"
           >
-            <span class="hidden lg:block">트윗</span>
-            <i class="fas fa-plus lg:hidden"></i>
+            <span class="hidden xl:block">트윗</span>
+            <i class="fas fa-plus xl:hidden"></i>
           </button>
         </div>
       </div>
       <!-- 프로필 버튼 -->
-      <div class="lg:mr-3 mb-3">
+      <div class="xl:mr-3 mb-3">
         <button
-          class="hidden lg:flex mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 items-center"
+          class="hidden xl:flex mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 items-center"
         >
           <!-- 100은 이미지 사이즈임  -->
           <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full" />
-          <div class="lg:ml-2 hidden lg:block">
+          <div class="xl:ml-2 hidden xl:block">
             <div class="text-sm font-bold">dmsdl8917</div>
             <div class="text-xs text-gray-500 text-left">id89189</div>
           </div>
           <i
-            class="ml-auto fas fa-ellipsis-h fa-fw text-xs hidden lg:block"
+            class="ml-auto fas fa-ellipsis-h fa-fw text-xs hidden xl:block"
           ></i>
         </button>
-        <div class="lg:hidden flex justify-center">
+        <div class="xl:hidden flex justify-center">
           <img
             src="http://picsum.photos/100"
             class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80"
