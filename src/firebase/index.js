df8790399a2f8dbf29158c396780ff6ceed9e097
joeuni <dmsdl8917@gmail.com>
2024-03-15 19:09:05 +0900

    파이어베이스 호스팅 설정 추가

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-q5Vp6Rovi434O5pKIpYWytR20vfbSGI",
+  authDomain: "twiiter-clone-joeuni.firebaseapp.com",
+  projectId: "twiiter-clone-joeuni",
+  storageBucket: "twiiter-clone-joeuni.appspot.com",
+  messagingSenderId: "24764184539",
+  appId: "1:24764184539:web:6981eeb91247ba6028d9fe",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
