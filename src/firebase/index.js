fe3c37cadd2a0d69d2bf051772a9e0f1e55f0a1d
joeuni <dmsdl8917@gmail.com>
2024-03-16 21:42:12 +0900

    firestore/db/storage 설정

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +18,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+//인증
+const auth = getAuth(app);
+
+//파이어스토어
+const db = getFirestore(app);
+
+//스토리지
+const storage = getStorage(app);
+
+export { auth, db, storage };
